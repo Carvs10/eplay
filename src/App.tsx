c25ef0231ba090e081f2d03dad09852c1780db47
Carvs10 <joaovcl10@hotmail.com>
2024-01-17 10:44:29 -0300

    config redux and reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import Header from './components/Header'
@@ -5,17 +6,20 @@ import { GlobalStyles } from './styles'
 
 import RoutesPages from './routes'
 import Footer from './components/Footer'
+import { store } from './store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyles />
-      <div className="container">
-        <Header />
-      </div>
-      <RoutesPages />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyles />
+        <div className="container">
+          <Header />
+        </div>
+        <RoutesPages />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
